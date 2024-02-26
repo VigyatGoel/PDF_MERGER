aa5af7a03725e25600d22eb5ffea93dd7e82d22f
Vigyat Goel <vigyatgoel@gmail.com>
2024-02-26 19:47:37 +0530

    Made the code more modular and added feature to name the merged file and to filter out all files other than pdfs

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 from PyPDF2 import PdfWriter, PdfReader
 import os
 
-# put all files in pdf_files folder
-# make sure to run this file in the same directory where the pdf_files folder is located
 
-pdf_files = os.listdir('pdf_files')
-merger = PdfWriter()
-
-specific_files = []
-
-n = int(input("Enter the number of files you want to merge or enter 0 to merge all files: "))
-
-if n != 0:
-    print("Enter the names of the files you want to merge")
-    for i in range(n):
-        file = input(f"Enter the name of the file {i + 1}: ")
-        specific_files.append(file)
-
-print("Merging PDFs...")
-
-if n == 0:
+def merge_pdfs(pdf_files, output_file):
+    merger = PdfWriter()
     for pdf in pdf_files:
-        pdf_reader = PdfReader(f'pdf_files/{pdf}')
+        pdf_reader = PdfReader(pdf)
         merger.append(pdf_reader)
+    try:
+        merger.write(output_file)
+        print("PDFs merged successfully")
+    except PermissionError:
+        print("Error: Permission denied")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        merger.close()
 
-else:
-    for i in specific_files:
-        pdf_reader = PdfReader(f'pdf_files/{i}')
-        merger.append(pdf_reader)
 
-try:
-    merger.write("merged-pdf.pdf")
-    print("PDFs merged successfully")
+def main():
+    pdf_files = os.listdir('pdf_files')
+    # Filter out non-PDF files
+    pdf_files = [file for file in pdf_files if file.endswith('.pdf')]
+    n = int(input("Enter the number of files you want to merge or enter 0 to merge all files: "))
+    if n != 0:
+        print("Enter the names of the files you want to merge")
+        specific_files = [input(f"Enter the name of the file {i + 1}: ") for i in range(n)]
+        pdf_files = [f'pdf_files/{file}' for file in specific_files]
+    else:
+        confirm = input("Are you sure you want to merge all files? (yes/no): ")
+        if confirm.lower() != 'yes':
+            print("Exiting...")
+            return
+    output_file = input("Enter the name of the output file: ")
+    merge_pdfs(pdf_files, output_file)
 
-except FileNotFoundError:
-    print("Error")
 
-finally:
-    merger.close()
+if __name__ == "__main__":
+    main()
